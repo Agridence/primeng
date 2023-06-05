cb91687ada3bb8eb7bdad0209b01878b2299bae7
vutruong <hoangvu.truong@agridence.com>
2023-06-05 18:36:33 +0800

    Adding server js file for deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+// NOTE: if you need to run helmet config/content security policy locally,
+// you should change port to 4200 to comply with CORS origins check by API backend
+const port = 8080;
+app.use(
+  "/",
+  express.static(__dirname + '/dist/primeng/browser')
+);
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", {
+    root: "dist/HeveaConnectPlatformFrontend/browser/",
+  });
+});
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server listening on port :${port}/`);
+});
